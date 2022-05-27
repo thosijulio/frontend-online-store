9e3f3c65c52b92386b737b88bc18436688a8e0dc
thosijulio <thosijulio@gmail.com>
2022-05-27 15:43:15 -0300

    feat-criacao-menu-categorias

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 import './ProductList.css';
 
 class ProductList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      selectedCategory: '',
+    }
+  }
+
+  componentDidMount() {
+    getCategories()
+      .then((categories) => {
+        this.setState({
+          categories,
+        });
+      });
+  }
+
+  changeSelectedCategory(selectedCategory) {
+    this.setState({
+      selectedCategory,
+    })
+  }
+
   render() {
+    const { state: { categories } } = this;
     return (
-      <header className="product-list-header">
-        <h1>Front-end Online Store</h1>
-        <i className="fa fa-search fa-2xl"/>
-        <input className="input-field" type="text" />
-        <Link to="/cart-shopping" className="fa fa-cart-shopping fa-2xl" />
-        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
-      </header>
+      <>
+        <header>
+          <h1>Front-end Online Store</h1>
+        </header>
+        <section className="product-list-section">
+          <div>
+            <i className="fa fa-search fa-2xl" />
+            <input className="input-field" type="text" />
+            <Link to="/cart-shopping" className="fa fa-cart-shopping fa-2xl" />
+            <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+          </div>
+          <aside className="product-categories">
+          <h3>Categorias:</h3>
+          { categories.map((category, index) => (
+            <label key={index}>
+              <input
+                name="selected-category"
+                onClick={({ target: { value } }) => this.changeSelectedCategory(value)}
+                type="radio"
+                value={category.id}
+              />
+              {category.name}
+            </label>
+          ))}
+        </aside>
+        </section>
+      </>
     )
   }
 }
